cd2bdc76313c49196a68d9afac882e8e5a7bb913
Nate <nate@enoch.(none)>
2010-11-05 22:34:59 -0400

    Initial Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CcTally::Application.config.secret_token = 'a2561806762ca8b3ed064e682869c7c054026cec561264863aa1a8bc8b1dc04fe0cbddd6779b2379a494182b290f1a3c631c9d0155029c47b2ef65d778594bde'
