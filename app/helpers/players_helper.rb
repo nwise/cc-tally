e235232d76fdf2df728adec0d7d9f27cba21b119
Nate <nate@enoch.(none)>
2010-12-27 10:06:59 -0500

    Fixing chart

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -12,16 +12,19 @@ module PlayersHelper
       x_labels << player.first_name
     end
     data = [wins, losses]
-    y_labels = [0, ((wins.max*0.25).ceil), ((wins.max*0.5).ceil), ((wins.max*0.75).ceil),wins.max]
+    game_totals = []
+    Player.all.each{|p| game_totals << Game.games_for_player(p).size}
+    total_games = game_totals.max
+    y_labels = [0, ((total_games*0.25).ceil), ((total_games*0.5).ceil), ((total_games*0.75).ceil),total_games]
     chart_hash = {:data => data,
-                  :stacked => false,
+                  #:stacked => false,
                   :title => "Wins/Losses by Player",
                   :axis_labels => [x_labels.join('|'), y_labels.join('|')], 
                   :axis_with_labels => 'x,y', 
                   :bar_colors => ['83e641', 'ff6619'],
                   :width => 400,
                   :height => 300,
-                  :custom => ''}
+                  :custom => 'chg=0,0,5,0'}
 
   end
 end
