01aea3da75200aba1382c64357700e2469d3a253
Nate <nate@enoch.(none)>
2010-12-27 10:48:23 -0500

    Adding updated charts

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,34 @@
 module GamesHelper
+
+  def total_player_wins_hash
+    wins = []
+    labels = []
+    Player.all.each do |p| 
+      win =  Game.wins_for_player(p)
+      wins << win
+      labels << "#{p.first_name} - #{win}"
+    end
+
+    {:data => wins,
+     :labels => labels,
+     :title => "Wins",
+     :width => 315,
+     :custom => "chco=336114"}
+  end
+
+  def total_player_losses_hash
+    losses = []
+    labels = []
+    Player.all.each do |p| 
+      loss = (Game.games_for_player(p).size - Game.wins_for_player(p))
+      losses << loss
+      labels << "#{p.first_name} - #{loss}"
+    end
+
+    {:data => losses,
+     :labels => labels,
+     :title => "Losses",
+     :width => 315,
+     :custom => "chco=ad421b"}
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,6 @@ class Player < ActiveRecord::Base
 
   has_many :games
 
+  default_scope :order => "id ASC"
+
 end
